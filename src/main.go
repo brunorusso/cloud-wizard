f869d2d2996bde7d2986372bc1a5f3160296a3fb
Bruno Tadeu Russo <brusso@gmail.com>
2022-07-19 22:58:43 -0300

    update

--- src/main.go
+++ src/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	//Input data to process
 	fmt.Println("Cloud Wizard")
 	reader := bufio.NewReader(os.Stdin)
 
@@ -44,12 +45,19 @@ func main() {
 	fmt.Print("Possui SNS? (s/n): ")
 	V_SNS, _ := reader.ReadString('\n')
 
-	fmt.Sprintf("SNS = ", V_SNS)
-	SNS := check_resposta(V_SNS)
-	fmt.Sprintf("SNS = ", SNS)
+	// Check Data
+	V_Front = check_resposta(V_Front)
+	V_API = check_resposta(V_API)
+	V_EKS = check_resposta(V_EKS)
+	V_ECS = check_resposta(V_ECS)
+	V_RDS = check_resposta(V_RDS)
+	V_DynamoDB = check_resposta(V_DynamoDB)
+	V_Lambda = check_resposta(V_Lambda)
+	V_SQS = check_resposta(V_SQS)
+	V_SNS = check_resposta(V_SNS)
+	
 
-
-	// Links
+	// Docs links
 	L_Front := "https://docs.aws.amazon.com/AmazonS3/latest/userguide/WebsiteHosting.html"
 	L_S3 := "https://docs.aws.amazon.com/AmazonS3/latest/userguide/WebsiteHosting.html"
 	L_API := "https://docs.aws.amazon.com/apigateway/latest/developerguide/index.html"
@@ -62,6 +70,7 @@ func main() {
 	L_SNS := "https://docs.aws.amazon.com/sns/latest/dg/index.html"
 	L_Cache := "https://docs.aws.amazon.com/sns/latest/dg/index.html"
 
+	// Image links
 	IMG_Front := "https://brunorusso.com.br/icons/Arch_Amazon-CloudFront_32.png"
 	IMG_S3 := "https://brunorusso.com.br/icons/Arch_Amazon-Simple-Storage-Service_32.png"
 	IMG_API := "https://brunorusso.com.br/icons/"
@@ -94,72 +103,70 @@ func main() {
 	f, err := os.Create(V_Arquivo)
     	check(err)
 
-    	w := bufio.NewWriter(f)
+    w := bufio.NewWriter(f)
 	//Head
-    	w.WriteString("<html><head><title>Cloud Wizard</title></head>")
-    	w.WriteString("<style>")
-    	w.WriteString(".cdn {background-color: #D4E6F1; text-align: center;}")
-    	w.WriteString(".comp {background-color: #D1F2EB; text-align: center;}")
-    	w.WriteString(".dados {background-color: #FCF3CF; text-align: center;}")
-    	w.WriteString(".integracao {background-color: #F6DDCC; text-align: center;}")
-    	w.WriteString(".governanca {background-color: #E8DAEF; text-align: center;}")
-    	w.WriteString("</style>")
-    	w.WriteString("</head>")
+	w.WriteString("<html><head><title>Cloud Wizard</title></head>")
+   	w.WriteString("<style>")
+   	w.WriteString(".cdn {background-color: #D4E6F1; text-align: center;}")
+   	w.WriteString(".comp {background-color: #D1F2EB; text-align: center;}")
+   	w.WriteString(".dados {background-color: #FCF3CF; text-align: center;}")
+   	w.WriteString(".integracao {background-color: #F6DDCC; text-align: center;}")
+   	w.WriteString(".governanca {background-color: #E8DAEF; text-align: center;}")
+	w.WriteString(".ativo a:link, a:visited, a:hover, a:active {color: black;}")
+	w.WriteString(".inativo a:link, a:visited, a:hover, a:active {color: #E8DAEF;}")
+   	w.WriteString("</style>")
+   	w.WriteString("</head>")
 	//Body
 	w.WriteString("<html><head><title>Cloud Wizard</title></head><body bgcolor=\"#E7E7E7\">")
 	w.WriteString("<center><table border=\"0\" width=\"90%\"><tr><td width=\"25%\"><img src=\"https://github.com/brunorusso/cloud-wizard/blob/develop/img/Cloud-Wizard-Logo.png\"></td>")
-	w.WriteString(fmt.Sprintf("<td><H1><center>Projeto: %s </H1></center></td></tr></table></center>", V_Projeto))
-    	w.WriteString("<br><br><hr>")
+	w.WriteString(fmt.Sprintf("<td><H1><center>Projeto: %s </H1></center></td></tr></table></center>", V_Projeto))    	
+	w.WriteString("<br><br><hr>")
 	//Content
-    	w.WriteString("<table border=\"1\" width=\"80%\">")
+    w.WriteString("<table border=\"1\" width=\"80%\">")
 	w.WriteString("<tr>")
 	w.WriteString("<th width=\"20%\">Camada</th>")
 	w.WriteString("<th width=\"80%\" colspan=\"3\">Recursos</th>")
 	w.WriteString("</tr>")
-    	w.WriteString("<tr>")
+    w.WriteString("<tr>")
 	w.WriteString("<td> <div class=\"cdn\">CDN e Aceleração de Conteúdo</div></td>")
 	w.WriteString(fmt.Sprintf("<td> <div class=\"cdn\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"> <br>CloudFront</a></div></td>", L_Front, IMG_Front))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"cdn\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>S3</a></div</td>", L_S3, IMG_S3))
-	w.WriteString(fmt.Sprintf("<td> <div class=\"cdn\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>APIGateway</a></div></td>", L_API, IMG_API))
-    	w.WriteString("</tr>")
-    	w.WriteString("<tr>")
+	w.WriteString(fmt.Sprintf("<td> <div class=\"cdn\"><a class=\"%s\" href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>APIGateway</a></div></td>", V_API, L_API, IMG_API))
+    w.WriteString("</tr>")
+    w.WriteString("<tr>")
 	w.WriteString("<td> <div class\"comp\">Computação</div></td>")
 	w.WriteString(fmt.Sprintf("<td> <div class=\"comp\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>EKS</a></div</td>", L_EKS, IMG_EKS))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"comp\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>ECS</a></div</td>", L_ECS, IMG_ECS))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"comp\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>Lambda</a></div</td>", L_Lambda, IMG_Lambda))
-    	w.WriteString("</tr>")
-    	w.WriteString("<tr>")
+    w.WriteString("</tr>")
+    w.WriteString("<tr>")
 	w.WriteString("<td><div class\"dados\">Banco de Dados e Cache</div></td>")
 	w.WriteString(fmt.Sprintf("<td> <div class=\"dados\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>RDS</a></div</td>", L_RDS, IMG_RDS))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"dados\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>DynamoDB</a></div</td>", L_DynamoDB, IMG_DynamoDB))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"dados\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>Cache</a></div</td>", L_Cache, IMG_Cache))
-    	w.WriteString("</tr>")
-    	w.WriteString("<tr>")
+    w.WriteString("</tr>")
+    w.WriteString("<tr>")
 	w.WriteString("<td><div class\"integracao\">Integração</div></td>")
 	w.WriteString(fmt.Sprintf("<td> <div class=\"integracao\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>SQS</a></div</td>", L_SQS, IMG_SQS))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"integracao\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"><br>SNS</a></div</td>", L_SNS, IMG_SNS))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"integracao\"><a href=\"%s\" target=\"_blank\"><img src=\"%s\"></a></div</td>"))
-    	w.WriteString("</tr>")
-    	w.WriteString("<tr>")
+    w.WriteString("</tr>")
+    w.WriteString("<tr>")
 	w.WriteString("<td><div class\"governanca\">Governança</div></td>")
 	w.WriteString(fmt.Sprintf("<td> <div class=\"governanca\"><a href=\"%s\" target=\"_blank\"><img src=\"\"></a></div</td>"))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"governanca\"><a href=\"%s\" target=\"_blank\"><img src=\"\"></a></div</td>"))
 	w.WriteString(fmt.Sprintf("<td> <div class=\"governanca\"><a href=\"%s\" target=\"_blank\"><img src=\"\"></a></div</td>"))
-    	w.WriteString("</tr>")
-    	w.WriteString("</table>")
+	w.WriteString("</tr>")
+    w.WriteString("</table>")
 
-		//Footer
-		w.WriteString(fmt.Sprintf("<center><h5>Wizard criado em: %s<h5></center>", V_Hoje))
+	//Footer
+	w.WriteString(fmt.Sprintf("<center><h5>Wizard criado em: %s<h5></center>", V_Hoje))
 
   	check(err)
-
     	w.WriteString("</body></html>")
     	check(err)
-
     	defer f.Close()
     	w.Flush()
-
-
 }
 
 func check(e error) {
@@ -169,11 +176,11 @@ func check(e error) {
 }
 
 
-func check_resposta(x string) {
+func check_resposta(x string) string {
 	if strings.Contains(x, "s"){
-		ativo := "ativo"
-		return ativo
+		V_ativo := "ativo"
+		return V_ativo
 	}
-
+	V_ativo := "inativo"
+	return V_ativo
 }
-
